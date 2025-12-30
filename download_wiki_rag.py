094f28bc16d763a8907dbd367d770176b87b0be6
jpcoder7 <wx6djq79vk@privaterelay.appleid.com>
2025-12-30 16:01:51 +0100

    Create download_wiki_rag.py

---
+++ download_wiki_rag.py
@@ -0,0 +1,23 @@
+# mkdir /workspace/rag/wiki
+
+python - << 'PY'
+import json
+from datasets import load_dataset
+
+wiki_data = "/workspace/rag/wiki/enwiki_20231101_preprocessed.jsonl"
+
+wiki_stream = load_dataset(
+    "wikimedia/wikipedia",
+    "20231101.en",
+    split="train",
+    streaming=True
+)
+
+with open(wiki_data, "w", encoding="utf-8") as f:
+    for i, row in enumerate(wiki_stream):
+        f.write(json.dumps({
+            "id": str(i),
+            "title": row.get("title", ""),
+            "text": row.get("text", "")
+        }, ensure_ascii=False) + "\n")
+PY
